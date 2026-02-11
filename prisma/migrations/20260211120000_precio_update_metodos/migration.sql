8fd2c75f412841a57e736b88cd0c808c5923c1e2
islaemanuel25-glitch <islaemanuel25@gmail.com>
2026-02-11 19:57:19 -0300

    fix migracion precio update metodos

--- prisma/migrations/20260211120000_precio_update_metodos/migration.sql
+++
@@ -1,3 +0,0 @@
--- Extiende los métodos disponibles para historial/aplicación de actualizaciones de precios
-ALTER TYPE "PrecioUpdateMetodo" ADD VALUE IF NOT EXISTS 'REGLAS';
-ALTER TYPE "PrecioUpdateMetodo" ADD VALUE IF NOT EXISTS 'PEGADO';
